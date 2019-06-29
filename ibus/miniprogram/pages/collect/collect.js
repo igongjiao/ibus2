16f30518c44b74a6ad82129508b3b5dbf0521f93
Chencpy6 <253545375@qq.com>
2019-06-29 11:06:02 +0800

    收藏页面跳转完成
    2019/6/29

--- ibus/miniprogram/pages/collect/collect.js
+++ ibus/miniprogram/pages/collect/collect.js
@@ -1,4 +1,7 @@
-var app = getApp()
+var app = getApp();
+var qqmapsdk = app.globalData.qqmapsdk;
+const util = require('../../utils/util.js');
+
 Page({
 
   /**
@@ -10,34 +13,88 @@ Page({
       "站点收藏",
       "路线收藏",
     ],
-    station: [{ message: "", collected: false}],
-    routine: [{ message: "", collected: false}],
+    station: [{
+      message: "",
+      collected: false
+    }],
+    routine: [{
+      message: "",
+      collected: false
+    }],
     windowHeight: 0,
-    scrollViewHeight:0,
-    
+    scrollViewHeight: 0,
+
     id: ''
   },
-  
-  switchPage: function (option) {
-    console.log(option);
+
+  switchStationPage: function(option) {
+    let _page = this;
+    let stationName = option.currentTarget.id;
+    //console.log(stationName);
+
+    app.globalData.qqmapsdk.search({
+      keyword: stationName,
+      region: "武汉市",
+      page_size: 10,
+      page_index: 1,
+      filter: encodeURI("category=公交车站"),
+      success: function(res) {
+        console.log(res);
+        let stationlist = [];
+        if (res.data.length > 0) {
+          stationlist.push({
+            title: res.data[0].title.split("[")[0],
+            city: res.data[0].ad_info.city,
+            bus: res.data[0].address,
+            location: res.data[0].location,
+          });
+          app.globalData.selectStation = stationlist[0];
+          wx.switchTab({
+            url: '../index/index',
+          })
+        }
+        console.log(_page.data.searchStation);
+      },
+      fail: function(error) {
+        util.logError("地点信息获取失败");
+        return;
+      }
+    });
   },
 
-  onLoad: function (option) {
+  switchBusPage: function (option) {
+    let _page = this;
+    let busName = option.currentTarget.id;
+    let busInfo = {
+      fullInfo: false,
+      title: busName,
+      city: "武汉市",
+      startStation: "",
+      endStation: "",
+    };
+    app.globalData.selectBus = busInfo;
+    wx.navigateTo({
+      url: '../bus_diagram/bus_diagram',
+    });
+
+  },
+
+  onLoad: function(option) {
     var that = this
     wx.getSystemInfo({
       success: function(res) {
         that.setData({
-          windowHeight:res.windowHeight
+          windowHeight: res.windowHeight
         })
       },
     })
-      console.log(that.data.windowHeight)
-      let scrollViewHeight = this.data.windowHeight - 50
-      this.setData({
-        scrollViewHeight: scrollViewHeight
-      })
+    console.log(that.data.windowHeight)
+    let scrollViewHeight = this.data.windowHeight - 50
+    this.setData({
+      scrollViewHeight: scrollViewHeight
+    })
   },
-  onShow: function (option) {
+  onShow: function(option) {
 
     const db = wx.cloud.database()
     var that = this
@@ -45,24 +102,23 @@ Page({
     db.collection('something').where({
       _openid: app.globalData.openid,
     }).get({
-      success: function (res) {
+      success: function(res) {
         console.log('成功')
         console.log(res.data.length)
-        var station=that.data.station
-        for (var i = 0; i < res.data.length; i++)
-        {
-          
+        var station = that.data.station
+        for (var i = 0; i < res.data.length; i++) {
+
           var key = "station[" + i + "].message"
           var key2 = "station[" + i + "].collected"
           console.log(res.data[i].station)
           that.setData({
             [key]: res.data[i].station,
-            [key2]:true
+            [key2]: true
           })
         }
-        
+
       },
-      fail: function (res) {
+      fail: function(res) {
         console.log()
       }
     })
@@ -70,7 +126,7 @@ Page({
     db.collection('route').where({
       _openid: app.globalData.openid,
     }).get({
-      success: function (res) {
+      success: function(res) {
         console.log('成功')
         console.log(res.data.length)
         var station = that.data.station
@@ -81,21 +137,21 @@ Page({
           console.log(res.data[i].route)
           that.setData({
             [key]: res.data[i].route,
-            [key2]:true
+            [key2]: true
           })
         }
 
       },
-      fail: function (res) {
+      fail: function(res) {
         console.log()
       }
     })
-   
+
   },
   /**
    * 点击导航栏
    */
-  onNavBarTap: function (e) {
+  onNavBarTap: function(e) {
     // 获取点击的navbar的index
     let navbarTapIndex = e.currentTarget.dataset.navbarIndex
     // 设置data属性中的navbarActiveIndex为当前点击的navbar
@@ -104,19 +160,21 @@ Page({
     })
   },
 
-  
+
 
   /**
    * 
    */
-  onBindAnimationFinish: function ({ detail }) {
+  onBindAnimationFinish: function({
+    detail
+  }) {
     // 设置data属性中的navbarActiveIndex为当前点击的navbar
     this.setData({
       navbarActiveIndex: detail.current
     })
   },
 
-  Collect_location: function (e) {
+  Collect_location: function(e) {
     const db = wx.cloud.database()
     const _ = db.command
     var station = this.data.station[e.currentTarget.dataset.index].message
@@ -132,11 +190,11 @@ Page({
       _openid: _.eq(app.globalData.openid),
       station: station
     }).get().then(res => {
-      
+
       console.log(res.data[0]._id);
       that.setData({
         id: res.data[0]._id,
-        [key]:false
+        [key]: false
       })
 
       console.log(that.data.station);
@@ -149,15 +207,15 @@ Page({
       });
     });
 
-   
+
 
     //console.log(this.data.id)
-    
 
-    
+
+
   },
 
-  Collect_route: function (e) {
+  Collect_route: function(e) {
     const db = wx.cloud.database()
     const _ = db.command
     var route = this.data.routine[e.currentTarget.dataset.index].message
